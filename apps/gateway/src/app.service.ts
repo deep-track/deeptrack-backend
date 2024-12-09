ce21df2bfa8974de434c396b91455cac2866624e
Harmaton <njagiiharmaton@gmail.com>
2024-12-09 12:50:01 +0300

    Transport Infrastructure Setup

--- apps/gateway/src/app.service.ts
+++ apps/gateway/src/app.service.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { UploadImageDto } from './app.dto';
+import { ClientKafka } from '@nestjs/microservices';
+import { ImageSubmittedEvent } from './image-submited';
+
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @Inject('IMAGES_SERVICE') private readonly imagesProxyClient: ClientKafka
+  ) {}
+
+  async processImage(file: Express.Multer.File, payload: UploadImageDto) {
+    try {      
+      // Emit the event to Kafka topic
+      this.imagesProxyClient.emit('image_submitted', new ImageSubmittedEvent(file, payload.filename ));
+      return {
+        status: 'Image submitted successfully',
+        filename: file.originalname
+      };
+    } catch (error) {
+      throw new Error(`Failed to process image: ${error.message}`);
+    }
   }
 }
--- apps/images-app/src/main.ts
+++ apps/images-app/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { ImagesAppModule } from './images-app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ImagesAppModule);
-  await app.listen(process.env.port ?? 3011);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(ImagesAppModule,
+    {
+      transport: Transport.KAFKA,
+      options: {
+        client: {
+          brokers: ['localhost:9092']
+        },
+        consumer: {
+          groupId: 'images-consumer'
+        }
+      }
+    }
+  );
+  await app.listen();
 }
 bootstrap();
